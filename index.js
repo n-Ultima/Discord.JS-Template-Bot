050292e9ce7909163539798f04a613ca8611510c
Nicolas Barclay <78455052+n-Ultima@users.noreply.github.com>
2021-05-17 01:34:36 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+Client.on('message' async(message) =>
+  if(message === '!ping'){
+    message.reply('Pong!');
+  }
+  if(message === '!ban'){
+    if(!message.author.hasPermission('BAN_MEMBERS`) return;
+    let user = message.mentions.first();
+    user.ban();
+  }
+)};
